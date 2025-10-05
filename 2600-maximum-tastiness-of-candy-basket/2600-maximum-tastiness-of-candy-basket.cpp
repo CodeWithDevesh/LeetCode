12a98414323ce7a2609dc12167340a5cae65da4a
Devesh Agarwal <70127472+CodeWithDevesh@users.noreply.github.com>
2025-10-05 18:41:29 +0530

    Time: 67 ms (7.73%), Space: 52.3 MB (24.26%) - LeetHub

---
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPossible(int diff, vector<int>& price, int k) {
+        int placed = 1;
+        int last = 0;
+        for (int i = 1; i < price.size(); i++) {
+            if (price[i] - price[last] >= diff){
+                placed++;
+                last = i;
+            }
+        }
+        if (placed >= k)
+            return true;
+        return false;
+    }
+
+    int maximumTastiness(vector<int>& price, int k) {
+        sort(price.begin(), price.end());
+        int n = price.size();
+        int l = 0, r = price[n - 1] - price[0];
+
+        while (r >= l) {
+            int mid = (r + l) / 2;
+            if (isPossible(mid, price, k))
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return r;
+    }
+};
