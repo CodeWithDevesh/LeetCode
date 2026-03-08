2fb902cc0056f022fbc9217830315d79640e38ce
Devesh Agarwal <70127472+CodeWithDevesh@users.noreply.github.com>
2026-03-08 14:50:02 +0530

    Time: 16 ms (7.59%), Space: 22.1 MB (11.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,11 +6,16 @@ public:
 
         for(int i = 0; i < intervals.size() - 1; i++){
             if(intervals[i][1] >= intervals[i+1][0]){
-                intervals[i][1] = max(intervals[i][1], intervals[i+1][1]);
-                intervals.erase(intervals.begin()+i+1);
-                i--;
+                intervals[i+1][0] = intervals[i][0];
+                intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1]);
+                intervals[i] = {-1, -1};
             }
         }
-        return intervals;
+        vector<vector<int>> ans;
+        for(auto x : intervals){
+            if(x[0] != -1)
+                ans.push_back(x);
+        }
+        return ans;
     }
 };
